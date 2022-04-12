3cbf3e7825568c13cbdd56a242fa227fd2eee10e
yazshrilld <yazshrilld@gmail.com>
2022-04-12 01:09:18 +0100

    new excercise

---
+++ MicroNauts/index.js
@@ -0,0 +1,18 @@
+//Excercise
+console.log("YazCodes, Hello World!");
+
+let myName = "YazCodes";
+console.log(myName);
+
+// Excercise 4
+
+let greeting = "Hello my name is";
+let myname = "YazCodes";
+let lastname = " Musa";
+let fullname = `${myname} ${lastname}`;
+let str = "And I have";
+let apples = 200;
+apples *= 2;
+let end = "apples";
+
+console.log(greeting, fullname, str, apples, end);
