88cffb9a07cee1ab8dea84c71749519664e7ff68
yazshrilld <yazshrilld@gmail.com>
2022-04-13 05:46:40 +0100

    prices

--- MicroNauts/index.js
+++ MicroNauts/index.js
@@ -1,18 +1,168 @@
-//Excercise
-console.log("YazCodes, Hello World!");
+// //Level -1
 
-let myName = "YazCodes";
-console.log(myName);
+// //Excercise
+// console.log("YazCodes, Hello World!");
 
-// Excercise 4
+// let myName = "YazCodes";
+// console.log(myName);
 
-let greeting = "Hello my name is";
-let myname = "YazCodes";
-let lastname = " Musa";
-let fullname = `${myname} ${lastname}`;
-let str = "And I have";
-let apples = 200;
-apples *= 2;
-let end = "apples";
+// // Excercise 4
 
-console.log(greeting, fullname, str, apples, end);
+// let greeting = "Hello my name is";
+// let myname = "YazCodes";
+// let lastname = " Musa";
+// let fullname = `${myname} ${lastname}`;
+// let str = "And I have";
+// let apples = 200;
+// apples *= 2;
+// let end = "apples";
+
+// console.log(greeting, fullname, str, apples, end);
+
+// //Level - 2
+
+// //Excerdise 2
+
+// console.log("Yazid");
+
+// let Y = "Green";
+// if(Y === "Blue") {
+//   console.log("Blue detected");
+// } else if(Y === "Green") {
+//   console.log("Green detected");
+// } else {
+//   console.log("No Green or Blue Detected");
+// }
+
+
+// //Excerdise 3
+
+// let n = 50;
+// if(n > 45) {
+//   if(n < 65 ) {
+//     console.log(n);
+//   }
+// }
+
+//Excerdise 4
+
+// let n = 1;
+// while(n < 9) {
+//   n++;
+//   console.log(n);
+// }
+
+//Excerdise 5
+
+// for(let n = 4; n <= 12; n++) {
+//     console.log(n);
+//   }
+
+//Excerdise 6
+
+// let arr = [8, 9, 10, 11, 12, 13, 14, 15];
+// console.log(arr[3]);
+// console.log(...arr);
+
+//Excerdise 7
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for(let n = 0; n < arr.length; n++) {
+//   if(n >= 4) {
+//     console.log(arr[n]);
+//   }
+// }
+
+//Free Code Camp challenges
+
+//1
+// function welcomeToBooleans() {
+//     // Only change code below this line
+  
+//     return true; // Change this line
+  
+//     // Only change code above this line
+//   }
+//   console.log(welcomeToBooleans());
+
+//4
+// Setup
+// function testEqual(val) {
+//     if (val == 12) { // Change this line
+//       return "Equal";
+//     }
+//     return "Not Equal";
+//   }
+  
+//   testEqual(10);
+
+//5
+// function testGreaterOrEqual(val) {
+//     if (val >= 20) {  // Change this line
+//       return "20 or Over";
+//     }
+  
+//     if (val >= 10) {  // Change this line
+//       return "10 or Over";
+//     }
+  
+//     return "Less than 10";
+//   }
+  
+//   testGreaterOrEqual(10);
+
+//6
+// Only change code below this line
+// const myArray = ["yazid", 28];
+
+//7 Access Array Data With Indexes
+// const myArray = [50, 60, 70];
+// const myData = myArray[0];
+
+//8 Modify Array Data With Indexes
+// const myArray = [18, 64, 99];
+
+// // Only change code below this line
+// myArray[0] = 45;
+
+//13
+// const myArray = [];
+
+// // Only change code below this line
+// let i = 5;
+// while( i >= 0 ) {
+//   myArray.push(i);
+//   i--;
+//   console.log(myArray);
+// }
+
+//14
+// const myArr = [2, 3, 4, 5, 6];
+
+// // Only change code below this line
+// let total = 0;
+// // Only change code below this line
+// for(let i = 0; i < myArr.length; i++) {
+//  total +=myArr[i];
+//   console.log(total);
+// }
+
+console.log("YazCodes");
+
+//Level 3
+//Excerdise 1
+
+// for( let i = 27; i < 103; i+=3 ) {
+//   console.log(i)
+// }
+
+//Excerdise 2
+
+// let arr = [];
+// for(let i = 3; i <= 12; i++) {
+//   // console.log(i);
+//   arr.push(i);
+//   // console.log(arr);
+// }
+// console.log(arr);
+// // let pops = arr.pop();
